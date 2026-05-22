e6c333daf7dd8efa2ccf7bb007837912578782dc
freesky <freesky_zh@hotmail.com>
2026-05-22 22:22:42 +0800

    Call GPTService in main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from src.services.gpt_service import GPTService
 
 if __name__ == "__main__":
     print("This is a LangChain Application")
+    gpt_service = GPTService()
+    response = gpt_service.ask("What is LangChain?")
+    print(response)
